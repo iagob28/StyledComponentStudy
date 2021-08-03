71146232f54fe58a416a082c42a23e75ce75f40a
iago.bortolon <iago_bortolon@hotmail.com>
2021-08-03 15:00:17 -0300

    Finalizado aula Styled Components

--- src/Components/imageFilter.js
+++ src/Components/imageFilter.js
@@ -4,15 +4,15 @@ import outros from "../assets/images/outros.svg";
 import saude from "../assets/images/saude.svg";
 import transporte from "../assets/images/transporte.svg";
 import utilidades from "../assets/images/utilidades.svg";
-import { Icone } from "../Components/UI";
+import { IconeTema } from "../Components/UI";
 
 export default (type) => {
   const Images = {
-    Restaurante: <Icone src={alimentacao} alt="Restaurante" />,
-    Saude: <Icone src={saude} alt="Saude" />,
-    Transporte: <Icone src={transporte} alt="Transporte" />,
-    Utilidades: <Icone src={utilidades} alt="Utilidades" />,
-    default: <Icone src={outros} alt="Outros" />,
+    Restaurante: <IconeTema src={alimentacao} alt="Restaurante" />,
+    Saude: <IconeTema src={saude} alt="Saude" />,
+    Transporte: <IconeTema src={transporte} alt="Transporte" />,
+    Utilidades: <IconeTema src={utilidades} alt="Utilidades" />,
+    default: <IconeTema src={outros} alt="Outros" />,
   };
   return Images[type] || Images.default;
 };
